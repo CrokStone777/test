3e1b47757ce69ed9eed86785631ca835dbe73b70
Maks <bigboy_2007@mail.ru>
2024-08-30 18:09:31 +0300

    usloviya

---
+++ bob.js
@@ -0,0 +1,36 @@
+
+// Условия
+
+// 1 - if else
+let a = 3;
+
+if (a > 4) {
+   console.log('Yes, Sir!')
+} else {
+   console.log('Noooo')
+};
+
+// 2 - тернарный оператор
+let b = 6;
+
+(b > 5) ? console.log('True') : console.log('False');
+
+// 3 - switch
+
+let c = 5;
+
+switch(c) {
+   case 4:
+      console.log('True');
+      break;
+   case 5:
+      console.log('True_2');
+      break;
+   default:
+      console.log('No_one_is_correct')
+      break;
+}
+
+
+                      
+                      
